d7c38fdad5c98e98e64482b44bddc9a5cef5bf80
misa arikawa <misaarikawa@MacBook-Pro-misaarikawa.local>
2016-07-01 15:39:49 +0900

    Revert "Revert "Correction all files for devise!""
    This reverts commit 947d41f9634a8f8e99b62e202374fc9dd130c3af.

--- app/assets/javascripts/bootstrap.js
+++ app/assets/javascripts/bootstrap.js
@@ -784,16 +784,12 @@ if (typeof jQuery === 'undefined') {
       var $this         = $(this)
       var $parent       = getParent($this)
       var relatedTarget = { relatedTarget: this }
-
       if (!$parent.hasClass('open')) return
-
       if (e && e.type == 'click' && /input|textarea/i.test(e.target.tagName) && $.contains($parent[0], e.target)) return
-console.log('読んでます')
 
       $parent.trigger(e = $.Event('hide.bs.dropdown', relatedTarget))
 
       if (e.isDefaultPrevented()) return
-
       $this.attr('aria-expanded', 'false')
       $parent.removeClass('open').trigger($.Event('hidden.bs.dropdown', relatedTarget))
     })
