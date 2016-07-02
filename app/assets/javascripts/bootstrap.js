1a9bf65bfaac4598e769d529740694f3c8e89a4a
misa arikawa <misaarikawa@MacBook-Pro-misaarikawa.local>
2016-07-02 12:30:30 +0900

    Corrected function of devise

--- app/assets/javascripts/bootstrap.js
+++ app/assets/javascripts/bootstrap.js
@@ -753,6 +753,7 @@ if (typeof jQuery === 'undefined') {
 
 +function ($) {
   'use strict';
+
   // DROPDOWN CLASS DEFINITION
   // =========================
 
@@ -784,12 +785,15 @@ if (typeof jQuery === 'undefined') {
       var $this         = $(this)
       var $parent       = getParent($this)
       var relatedTarget = { relatedTarget: this }
+
       if (!$parent.hasClass('open')) return
+
       if (e && e.type == 'click' && /input|textarea/i.test(e.target.tagName) && $.contains($parent[0], e.target)) return
 
       $parent.trigger(e = $.Event('hide.bs.dropdown', relatedTarget))
 
       if (e.isDefaultPrevented()) return
+
       $this.attr('aria-expanded', 'false')
       $parent.removeClass('open').trigger($.Event('hidden.bs.dropdown', relatedTarget))
     })
--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,5 @@ Rails.application.routes.draw do
   get  '/prototypes/index'  =>    'prototypes#index'
   get  '/prototypes/new'    =>    'prototypes#new'
   get  '/prototypes/show'   =>    'prototypes#show'
-  get  '/users/:id'        =>    'users#show'
+  get  '/users/:id'         =>    'users#show'
 end
