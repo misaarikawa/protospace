85982bc5db339778ae76b8bd4a917546313321f3
misa arikawa <misaarikawa@MacBook-Pro-misaarikawa.local>
2016-07-07 11:52:24 +0900

    Add root_path about users

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,15 @@
 Rails.application.routes.draw do
+  resources :users, only: [:show, :edit, :update]
   devise_for :users
 
-  resources :users, only: [:show, :edit, :update]
 
   root 'prototypes#index'
 
-  get  '/prototypes/index'  =>    'prototypes#index'
-  get  '/prototypes/new'    =>    'prototypes#new'
-  get  '/prototypes/show'   =>    'prototypes#show'
-  get  '/users/:id'         =>    'users#show'
+  get   '/prototypes/index'  =>    'prototypes#index'
+  get   '/prototypes/new'    =>    'prototypes#new'
+  get   '/prototypes/show'   =>    'prototypes#show'
+  get   '/users/:id'         =>    'users#show'
+  get   '/users/:id/edit'    =>    'users#edit'
+  patch '/users/:id'         =>    'users#update'
+
 end
